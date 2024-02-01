1e5a3fa6dd42ffbce5ff4f5e2062e90f41eb6258
Riley <wardriley1@gmail.com>
2024-02-01 13:36:35 +0000

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div>
+    <header>
+      <h1>Blog Project</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/posts">Posts</a>
+      </nav>
+    </header>
+      <h2>Home</h2>
+      <p>This is the home page</p>
+    </div>
+  );
+}
---
+++ src/app/posts/page.js
@@ -0,0 +1,49 @@
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Posts() {
+  // we are sorting, becuase vercel puts the last one to change at the end...
+  const posts = await sql`SELECT * FROM posts ORDER BY id`;
+
+  async function handleCreatePost(formData) {
+    "use server";
+    const title = formData.get("title");
+    const content = formData.get("content");
+
+    await sql`INSERT INTO posts (title, content) VALUES (${title}, ${content})`;
+
+    revalidatePath("/posts");
+    // redirect("/posts"); // doesn't make sense here, because this is the page with the form
+  }
+
+  return (
+    <div>
+    <header>
+      <h1>Blog Project</h1>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Posts</a>
+      </nav>
+    </header>
+
+      <h2>Posts</h2>
+      <form action={handleCreatePost}>
+        <h4>Add a new post</h4>
+        <input name="title" placeholder="Blog Title" />
+        <textarea name="content" placeholder="Blog content"></textarea>
+        <button>Submit</button>
+      </form>
+
+      {posts.rows.map((post) => {
+        return (
+          <Link key={post.title} href={`/posts/${post.id}`}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
